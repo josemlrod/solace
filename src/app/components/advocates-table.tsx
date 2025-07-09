c8141e9c284d68da2520cf910fcc05a5f55cef12
josemlrod <mrjosemlrod@gmail.com>
2025-07-09 15:39:11 -0400

    add empty state

--- src/app/components/advocates-table.tsx
+++ src/app/components/advocates-table.tsx
@@ -34,64 +34,72 @@ export default function AdvocatesTable({
 
   return (
     <section className="bg-gray-200 overflow-auto border border-black rounded-md">
-      <table className="w-full">
-        <thead className="sticky top-0 bg-gray-300">
-          <tr>
-            <th className="border border-black rounded-md border-t-0 border-l-0">
-              First Name
-            </th>
-            <th className="border border-black rounded-md border-t-0">
-              Last Name
-            </th>
-            <th className="border border-black rounded-md border-t-0">City</th>
-            <th className="border border-black rounded-md border-t-0">
-              Degree
-            </th>
-            <th className="border border-black rounded-md border-t-0">
-              Specialties
-            </th>
-            <th className="border border-black rounded-md border-t-0">
-              Years of Experience
-            </th>
-            <th className="border border-black rounded-md border-t-0">
-              Phone Number
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredAdvocates.map((advocate: Advocate, index: number) => {
-            return (
-              <tr key={index}>
-                <td className="border border-black rounded-md text-center border-l-0">
-                  {advocate.firstName}
-                </td>
-                <td className="border border-black rounded-md text-center">
-                  {advocate.lastName}
-                </td>
-                <td className="border border-black rounded-md text-center">
-                  {advocate.city}
-                </td>
-                <td className="border border-black rounded-md text-center">
-                  {advocate.degree}
-                </td>
-                <td className="border border-black rounded-md p-2">
-                  <ul className="list-disc list-inside">
-                    {advocate.specialties.map((s: string, index: number) => (
-                      <li key={index}>{s}</li>
-                    ))}
-                  </ul>
-                </td>
-                <td className="border border-black rounded-md text-center">
-                  {advocate.yearsOfExperience}
-                </td>
-                <td className="border border-black rounded-md text-center">
-                  {advocate.phoneNumber}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {filteredAdvocates.length === 0 ? (
+        <div className="w-full min-h-80 flex items-center justify-center">
+          <p>No advocates found</p>
+        </div>
+      ) : (
+        <table className="w-full">
+          <thead className="sticky top-0 bg-gray-300">
+            <tr>
+              <th className="border border-black rounded-md border-t-0 border-l-0">
+                First Name
+              </th>
+              <th className="border border-black rounded-md border-t-0">
+                Last Name
+              </th>
+              <th className="border border-black rounded-md border-t-0">
+                City
+              </th>
+              <th className="border border-black rounded-md border-t-0">
+                Degree
+              </th>
+              <th className="border border-black rounded-md border-t-0">
+                Specialties
+              </th>
+              <th className="border border-black rounded-md border-t-0">
+                Years of Experience
+              </th>
+              <th className="border border-black rounded-md border-t-0">
+                Phone Number
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredAdvocates.map((advocate: Advocate, index: number) => {
+              return (
+                <tr key={index}>
+                  <td className="border border-black rounded-md text-center border-l-0">
+                    {advocate.firstName}
+                  </td>
+                  <td className="border border-black rounded-md text-center">
+                    {advocate.lastName}
+                  </td>
+                  <td className="border border-black rounded-md text-center">
+                    {advocate.city}
+                  </td>
+                  <td className="border border-black rounded-md text-center">
+                    {advocate.degree}
+                  </td>
+                  <td className="border border-black rounded-md p-2">
+                    <ul className="list-disc list-inside">
+                      {advocate.specialties.map((s: string, index: number) => (
+                        <li key={index}>{s}</li>
+                      ))}
+                    </ul>
+                  </td>
+                  <td className="border border-black rounded-md text-center">
+                    {advocate.yearsOfExperience}
+                  </td>
+                  <td className="border border-black rounded-md text-center">
+                    {advocate.phoneNumber}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </section>
   );
 }
